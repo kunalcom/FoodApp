90d7d08a5554d03328f9ff7c638a5e314469fbed
Kunal Kumar <krishnakk9091@gmail.com>
2025-11-14 16:04:57 +0530

    Update menu link to new URL

--- src/components/Layouts/Header.js
+++ src/components/Layouts/Header.js
@@ -37,7 +37,7 @@ const Header = () => {
               <Nav.Link as={Link} to="/about">
                 About
               </Nav.Link>
-              <Nav.Link as={Link} to="https://order-whirl-menu.lovable.app/">
+              <Nav.Link as={Link} to="https://menu-theta-seven.vercel.app/">
                 Our Menu
               </Nav.Link>
               <Nav.Link as={Link} to="/shop">
